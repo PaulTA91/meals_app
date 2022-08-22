168a2bd77605710573c051a3d2b92c3eb514d01e
PaulTA91 <parmst202@caledonian.ac.uk>
2022-08-22 13:05:38 +0100

    Lesson 165: Named Routes and passing Data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryID;
-  final String categoryTitle;
+  // final String categoryID;
+  // final String categoryTitle;
 
-  CategoryMealsScreen(this.categoryID, this.categoryTitle);
+  // CategoryMealsScreen(this.categoryID, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final categoryTitle = routeArgs['title'];
+    final categoryID = routeArgs['id'];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
