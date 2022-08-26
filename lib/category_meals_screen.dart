8f8cbf794e593b7ce301933b72ce2fbf6f27b15c
PaulTA91 <parmst202@caledonian.ac.uk>
2022-08-26 15:28:58 +0100

    LEsson 167: Meal model and data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -19,8 +19,8 @@ class CategoryMealsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text("Recipes for $categoryTitle go here."),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {}, itemCount: ,
       ),
     );
   }
